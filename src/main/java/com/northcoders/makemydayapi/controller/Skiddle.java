33e3abdf3293139f1d54b7fb0644071aee19af07
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-26 21:53:59 +0100

    implement getAllEvents from skiddle

---
+++ src/main/java/com/northcoders/makemydayapi/controller/Skiddle.java
@@ -0,0 +1,34 @@
+package com.northcoders.makemydayapi.controller;
+
+
+import com.northcoders.makemydayapi.model.SkiddleEvent;
+import com.northcoders.makemydayapi.service.SkiddleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/skiddle")
+public class Skiddle {
+
+    @Autowired
+    private SkiddleService skiddleService;
+
+    @GetMapping("/events")
+    public ResponseEntity<List<SkiddleEvent>> getAllEvents() {
+        try {
+            List<SkiddleEvent> events = skiddleService.getAllEvents();
+            return new ResponseEntity<>(events, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+
+}
---
+++ src/main/java/com/northcoders/makemydayapi/model/SkiddleVenue.java
@@ -0,0 +1,20 @@
+package com.northcoders.makemydayapi.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class SkiddleVenue {
+
+    private Long id;
+    private String name;
+    private String address;
+    private String town;
+    private String postcode;
+    private String country;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String type;
+
+}
