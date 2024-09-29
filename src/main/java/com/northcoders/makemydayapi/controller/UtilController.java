d8cf66ab85c06fd7e1bbc2ff972eeb097e990e04
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-29 18:34:53 +0100

    Add controller

---
+++ src/main/java/com/northcoders/makemydayapi/controller/UtilController.java
@@ -0,0 +1,22 @@
+package com.northcoders.makemydayapi.controller;
+
+import com.northcoders.makemydayapi.service.PopulateDatabaseScheduler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/utils")
+public class UtilController {
+
+    @Autowired
+    private PopulateDatabaseScheduler populateDatabaseScheduler;
+
+
+    @GetMapping("/skiddle/populatedb")
+    public String triggerPopulateDatabase(){
+        this.populateDatabaseScheduler.populateDatabaseForSkiddle();
+        return "Populated Database";
+    }
+}
