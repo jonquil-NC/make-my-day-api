9afb2f99c49113dd2f99aae87a067ada45c1c0da
fabiopulvirenti <fpulvirenti002@gmail.com>
2024-09-29 10:55:25 +0100

    feat:solving conflicts

--- src/main/java/com/northcoders/makemydayapi/service/SkiddleService.java
+++ src/main/java/com/northcoders/makemydayapi/service/SkiddleService.java
@@ -1,3 +1,10 @@
 package com.northcoders.makemydayapi.service;
+
+import com.northcoders.makemydayapi.model.SkiddleEvent;
+
+import java.util.List;
+
 public interface SkiddleService {
+
+    List<SkiddleEvent> getAllEvents();
 }
