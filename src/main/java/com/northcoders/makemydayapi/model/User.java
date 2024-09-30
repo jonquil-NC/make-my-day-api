35096c8bbdfd0d6ef7abce98893abe4168703f1c
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-30 15:55:58 +0100

    feat: update User entity class to include email and change firstName and lastName attributes to firstname and lastname

--- src/main/java/com/northcoders/makemydayapi/model/User.java
+++ src/main/java/com/northcoders/makemydayapi/model/User.java
@@ -18,11 +18,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String firstName;
-    private String lastName;
+    private String firstname;
+    private String lastname;
 
     @Column(unique = true)
     private String username;
+    private String email;
     private String password;
 
 }
