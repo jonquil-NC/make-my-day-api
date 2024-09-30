56ae5616f47786cbd6716d1bfb2a9782589f4fc2
ag-af <agabialves11@gmail.com>
2024-09-30 16:06:51 +0100

    feat: Implement Ticketmaster GET events by type.

--- src/main/java/com/northcoders/makemydayapi/controller/TicketmasterController.java
+++ src/main/java/com/northcoders/makemydayapi/controller/TicketmasterController.java
@@ -7,20 +7,38 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/activities/ticketmaster")
+@RequestMapping("api/v1/ticketmaster")
 public class TicketmasterController {
 
     @Autowired
     TicketmasterService ticketmasterService;
 
+    //Defining event types (Ticketmaster keywords)
+    private static final List<String> EVENT_TYPES = List.of("museum", "concert", "live events", "shows");
+
     @GetMapping("/events")
-    public ResponseEntity<List<Activity>> getAllEvents() {
-        List<Activity> events = ticketmasterService.getAllEvents();
-        return new ResponseEntity<>(events, HttpStatus.OK);
+    public ResponseEntity<List<Activity>> getEventsByType(@RequestParam String eventType) {
+
+        //Validate event type is in the list
+        if (!EVENT_TYPES.contains(eventType.toLowerCase())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        try {
+            List<Activity> activities = ticketmasterService.getEventsByKeyword(eventType);
+            if (activities.isEmpty()) {
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            }
+            return new ResponseEntity<>(activities, HttpStatus.OK);
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
+        }
     }
 }
