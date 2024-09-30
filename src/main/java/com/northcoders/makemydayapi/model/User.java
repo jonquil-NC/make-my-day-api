d0e4106df7378f29e5de2db1f075b8136b665377
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-30 15:28:19 +0100

    feat: add User entity class with lombok dependency

---
+++ src/main/java/com/northcoders/makemydayapi/model/User.java
@@ -0,0 +1,28 @@
+package com.northcoders.makemydayapi.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true)
+    private String username;
+    private String password;
+
+}
