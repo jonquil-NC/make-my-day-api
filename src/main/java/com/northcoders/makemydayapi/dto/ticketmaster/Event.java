6039ff7d0a5524c9b4ec72806e0359011d2df91b
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-10-01 10:00:34 +0100

    fix: remove unnecessary fields within Event class

--- src/main/java/com/northcoders/makemydayapi/dto/ticketmaster/Event.java
+++ src/main/java/com/northcoders/makemydayapi/dto/ticketmaster/Event.java
@@ -1,23 +1,15 @@
 package com.northcoders.makemydayapi.dto.ticketmaster;
 
-import com.google.gson.annotations.SerializedName;
 
 import java.util.List;
 
 public class Event {
 
     private String name;
-    private String type;
-    private String id;
     private List<Image> images;
-//    may not need Sales as we have dates
-//    private Sales sales;
+
     private Dates dates;
 
-   @SerializedName("info")
-    private String info;
-
-//   add PriceRanges class
     private String priceRanges;
 
     public String getName() {
@@ -28,21 +20,6 @@ public class Event {
         this.name = name;
     }
 
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
 
     public List<Image> getImages() {
         return images;
@@ -52,13 +29,6 @@ public class Event {
         this.images = images;
     }
 
-//    public Sales getSales() {
-//        return sales;
-//    }
-//
-//    public void setSales(Sales sales) {
-//        this.sales = sales;
-//    }
 
     public Dates getDates() {
         return dates;
@@ -68,13 +38,6 @@ public class Event {
         this.dates = dates;
     }
 
-    public String getInfo() {
-        return info;
-    }
-
-    public void setInfo(String info) {
-        this.info = info;
-    }
 
     public String getPriceRanges() {
         return priceRanges;
