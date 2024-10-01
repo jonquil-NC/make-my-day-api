0365d513786dce4dba0175156c3b2b4e32ef2fb9
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-10-01 12:04:13 +0100

    feat: add lombok builder annotation to User class and tidy up code - removing extra whitespace

--- src/main/java/com/northcoders/makemydayapi/model/User.java
+++ src/main/java/com/northcoders/makemydayapi/model/User.java
@@ -1,10 +1,7 @@
 package com.northcoders.makemydayapi.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
@@ -12,16 +9,18 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String firstname;
     private String lastname;
-
     private String email;
     private String password;
 
+
 }
+
+
