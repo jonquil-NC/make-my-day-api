db7cd6492781affa6677959720dda4f56b51b67e
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-10-02 07:58:07 +0100

    test: implement logic to pass signUp_whenUserAlreadyExists_shouldThrowUserAlreadyExistsException unit test for user service

--- src/test/java/com/northcoders/makemydayapi/usertest/UserServiceTests.java
+++ src/test/java/com/northcoders/makemydayapi/usertest/UserServiceTests.java
@@ -2,7 +2,9 @@ package com.northcoders.makemydayapi.usertest;
 
 import com.northcoders.makemydayapi.dto.AuthRequestDTO;
 import com.northcoders.makemydayapi.dto.UserInfoDTO;
+import com.northcoders.makemydayapi.exception.UserAlreadyExistsException;
 import com.northcoders.makemydayapi.mapper.AuthRequestMapper;
+import com.northcoders.makemydayapi.model.User;
 import com.northcoders.makemydayapi.repository.UserRepository;
 import com.northcoders.makemydayapi.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
@@ -11,9 +13,11 @@ import org.mockito.Mock;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.Optional;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @DataJpaTest
@@ -48,6 +52,27 @@ public class UserServiceTests {
         assertThat(actualResult).usingRecursiveComparison().isEqualTo(expectedResult);
 
 
+    }
+
+    @Test
+    public void signUp_whenUserAlreadyExists_shouldThrowUserAlreadyExistsException() {
+
+        var request = new AuthRequestDTO("John", "Doe", "john_doe@gmail.com", "password");
+
+        var existingUser = new User(1L, "Murphy", "Denny", "john_doe@gmail.com", "password");
+
+        when(mockUserRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(existingUser));
+
+        Exception e = assertThrows(UserAlreadyExistsException.class, () -> {
+            userServiceImpl.signUp(request);
+        });
+
+        String expectedMessage = "User with email " + request.getEmail() + " already exists!";
+        String actualMessage = e.getMessage();
+
+        assertTrue(actualMessage.contentEquals(expectedMessage));
+
+        verify(mockUserRepository, never()).save(any(User.class));
     }
 
 }
