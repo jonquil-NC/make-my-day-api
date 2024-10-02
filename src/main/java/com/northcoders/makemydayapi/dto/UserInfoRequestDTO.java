6c65f8d5a61086ee25fd31fa91da1f8097eafaf8
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-10-02 07:16:18 +0100

    feat: add NoArgsConstructor and Setter lombok annotations

--- src/main/java/com/northcoders/makemydayapi/dto/UserInfoRequestDTO.java
+++ src/main/java/com/northcoders/makemydayapi/dto/UserInfoRequestDTO.java
@@ -2,9 +2,14 @@ package com.northcoders.makemydayapi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @AllArgsConstructor
+@NoArgsConstructor
+
 public class UserInfoRequestDTO {
 
     private String firstname;
