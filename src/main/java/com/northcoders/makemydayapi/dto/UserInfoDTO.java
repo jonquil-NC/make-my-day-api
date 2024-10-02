9aae64c20d07e5c6097cf9f676b77a3c147e4f03
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-10-02 07:21:49 +0100

    feat: change name of class from UserInfoRequestDTO to UserInfoDTO

---
+++ src/main/java/com/northcoders/makemydayapi/dto/UserInfoDTO.java
@@ -0,0 +1,18 @@
+package com.northcoders.makemydayapi.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserInfoDTO {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+}
