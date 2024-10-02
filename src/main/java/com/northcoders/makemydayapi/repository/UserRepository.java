7b182bf292e02e3b154efde4d0c3b8cc7cbbfbc9
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-10-02 07:18:06 +0100

    feat: add findByEmail method to UserRepository class

--- src/main/java/com/northcoders/makemydayapi/repository/UserRepository.java
+++ src/main/java/com/northcoders/makemydayapi/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.northcoders.makemydayapi.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
