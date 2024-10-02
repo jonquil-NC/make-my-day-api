23ac93b15e17861136e42b917db9fc414c74572d
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-10-02 19:13:54 +0100

    feat: add service annotations to enable the application to run

--- src/main/java/com/northcoders/makemydayapi/MakemydayapiApplication.java
+++ src/main/java/com/northcoders/makemydayapi/MakemydayapiApplication.java
@@ -3,11 +3,25 @@ package com.northcoders.makemydayapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
+
 @SpringBootApplication
 public class MakemydayapiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MakemydayapiApplication.class, args);
+
+        String dataParsing = "2024-10-16";
+
+        try {
+            LocalDate parsedDate = LocalDate.parse(dataParsing);
+            System.out.println("parsed date is " + parsedDate);
+        } catch (DateTimeException e) {
+            System.err.println("Error parsing data" + e.getMessage());
+        }
+
+
     }
 
 }
