be74070e4451c665e20bcb9a416218a8fda0d4be
Cevdet Çağdaş Köse <chatonode@gmail.com>
2024-10-04 11:27:39 +0100

    refactor: add validation annotation to controller, remove extra sub-path from get mapping annotation

--- src/main/java/com/northcoders/makemydayapi/controller/TicketmasterController.java
+++ src/main/java/com/northcoders/makemydayapi/controller/TicketmasterController.java
@@ -4,6 +4,7 @@ import com.northcoders.makemydayapi.model.Activity;
 import com.northcoders.makemydayapi.model.ActivityType;
 import com.northcoders.makemydayapi.service.TicketmasterService;
 import com.northcoders.makemydayapi.service.TicketmasterServiceImpl;
+import com.northcoders.makemydayapi.validation.constraints.ValidTicketmasterActivityType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,45 +22,12 @@ public class TicketmasterController {
     @Autowired
     TicketmasterService ticketmasterService;
 
-    public TicketmasterController(TicketmasterServiceImpl ticketmasterService){
-        this.ticketmasterService = ticketmasterService;
-    }
+    @GetMapping("/events")
+    public ResponseEntity<List<Activity>> getEventsByActivityType(@RequestParam @ValidTicketmasterActivityType ActivityType activityType){
 
-    @GetMapping("ticketmaster/events")
-    public ResponseEntity<List<Activity>> getEventsByActivityType(@RequestParam ActivityType activityType){
         List<Activity> filteredTicketmasterEvents = ticketmasterService.getEventsByActivityType(activityType);
+
         return new ResponseEntity<>(filteredTicketmasterEvents, HttpStatus.OK);
     }
 
-    //Defining event types (Ticketmaster keywords)
-//    private static final List<String> EVENT_TYPES = List.of("museum", "concert", "live events", "shows");
-
-
-//    @GetMapping("/events")
-//    public ResponseEntity<List<Activity>> getAllEvents() {
-//
-//        List<Activity> ticketmasterEvents = ticketmasterService.getAllEvents();
-//
-//        return new ResponseEntity<>(ticketmasterEvents, HttpStatus.OK);
-//    }
-
-//    TODO
-//    @GetMapping("/events")
-//    public ResponseEntity<List<Activity>> getEventsByType(@RequestParam String eventType) {
-//
-//        //Validate event type is in the list
-//        if (!EVENT_TYPES.contains(eventType.toLowerCase())) {
-//            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-//        }
-//
-//        try {
-//            List<Activity> activities = ticketmasterService.getEventsByKeyword(eventType);
-//            if (activities.isEmpty()) {
-//                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-//            }
-//            return new ResponseEntity<>(activities, HttpStatus.OK);
-//        } catch (IOException e) {
-//            return new ResponseEntity<>(HttpStatus.SERVICE_UNAVAILABLE);
-//        }
-//    }
 }
