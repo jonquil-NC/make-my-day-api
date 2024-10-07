6f77b4a92c051b6fab0bfeaa70140172ca5b7ecd
Despina Ait-Amer <despinaaitamer@Despinas-MacBook-Air.local>
2024-10-07 12:25:43 +0100

    feat: add GlobalExceptionHandler class with custom messages

---
+++ src/main/java/com/northcoders/makemydayapi/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.northcoders.makemydayapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidEmailException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ApiErrorResponse> handleInvalidEmailException(InvalidEmailException ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse("Invalid email. Please try again.", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ResponseEntity<ApiErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse("User already exists. Please login with your existing username and password.", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ResponseEntity<ApiErrorResponse> handleGeneralException(Exception ex) {
+        ApiErrorResponse errorResponse = new ApiErrorResponse("It's not you - it's us!", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
